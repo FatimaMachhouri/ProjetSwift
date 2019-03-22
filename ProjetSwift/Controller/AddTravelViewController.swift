6548477996e017e179f82a8e666c4f25b23f1aaf
FatimaMachhouri <fatima.machhouri1@gmail.com>
2019-03-22 15:35:59 +0100

    add cancel action AddTravelView

---
+++ ProjetSwift/Controller/AddTravelViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddTravelController.swift
+//  ProjetSwift
+//
+//  Created by Fatima Machhouri on 22/03/2019.
+//  Copyright © 2019 F&Y. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddTravelViewController: UIViewController {
+
+    @IBAction func precedentAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
