522c5dfe2178ada22115debd87cc7d8eb288d86a
FatimaMachhouri <fatima.machhouri1@gmail.com>
2019-03-23 15:13:40 +0100

    add precedentAction on AddPersonView

---
+++ ProjetSwift/Controller/AddPersonViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddPersonViewController.swift
+//  ProjetSwift
+//
+//  Created by Fatima Machhouri on 23/03/2019.
+//  Copyright © 2019 F&Y. All rights reserved.
+//
+
+import UIKit
+
+class AddPersonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func precedentAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
