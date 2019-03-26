e1c34e7bfe4a5681e3eb40547d62bab061c631c1
FatimaMachhouri <fatima.machhouri1@gmail.com>
2019-03-26 17:17:18 +0100

    Add persontable view to add travel view

--- ProjetSwift/Controller/AddTravelViewController.swift
+++ ProjetSwift/Controller/AddTravelViewController.swift
@@ -9,11 +9,13 @@
 import Foundation
 import UIKit
 
-class AddTravelViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+class AddTravelViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDataSource {
+ 
     @IBOutlet weak var travelNameLabel: UITextField!
     @IBOutlet weak var personNameLabel: UITextField!
     var travelPic: UIImage? = nil
+    @IBOutlet weak var personTableView: UITableView!
+    var persons: [String] = []
     
     @IBAction func loadPhotoAction(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary) {
@@ -32,6 +34,36 @@ class AddTravelViewController: UIViewController, UITextFieldDelegate, UIImagePic
         picker.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func addPersonAction(_ sender: Any) {
+        guard let txt = self.personNameLabel.text else { return }
+        self.persons.append(txt)
+        self.personNameLabel.text = nil
+        addPersonToTableView(at: IndexPath(row: self.persons.count-1, section: 0))
+    }
+    
+    func addPersonToTableView(at indexPath: IndexPath) {
+        self.personTableView.beginUpdates()
+        self.personTableView.insertRows(at: [indexPath], with: UITableView.RowAnimation.middle)
+        self.personTableView.endUpdates()
+    }
+    
+    func deletePersonFromTableView(at indexPath: IndexPath) {
+        self.personTableView.beginUpdates()
+        self.personTableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.middle)
+        self.personTableView.endUpdates()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCell.EditingStyle.delete) {
+            self.persons.remove(at: indexPath.row)
+            self.deletePersonFromTableView(at: indexPath)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
     @IBAction func precedentAction(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
@@ -40,5 +72,16 @@ class AddTravelViewController: UIViewController, UITextFieldDelegate, UIImagePic
         textField.resignFirstResponder()
         return true
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.persons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
+        cell.textLabel?.text = self.persons[indexPath.row]
+        return cell
+    }
+    
+    
 }
