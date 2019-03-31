a6f120571f90f7d622c39d122d7816fb8d3d7a7b
FatimaMachhouri <fatima.machhouri1@gmail.com>
2019-03-31 03:25:36 +0200

    begin add expense feature

---
+++ ProjetSwift/Controller/AddExpense/AddExpenseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddExpenseViewController.swift
+//  ProjetSwift
+//
+//  Created by Fatima Machhouri on 31/03/2019.
+//  Copyright © 2019 F&Y. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddExpenseViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func precedentAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
---
+++ ProjetSwift/Controller/Cell/AddExpenseTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  AddExpenseTableViewCell.swift
+//  ProjetSwift
+//
+//  Created by Fatima Machhouri on 31/03/2019.
+//  Copyright © 2019 F&Y. All rights reserved.
+//
+
+import UIKit
+
+class AddExpenseTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var personNameLabel: UILabel!
+    @IBOutlet weak var buttonCheckBox: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
