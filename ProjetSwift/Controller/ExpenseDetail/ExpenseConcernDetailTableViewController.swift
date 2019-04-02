3204abebfdba3deee5feb0961a35f2cb3ff76bae
FatimaMachhouri <fatima.machhouri1@gmail.com>
2019-04-02 23:24:44 +0200

    finish Expense detail View

---
+++ ProjetSwift/Controller/ExpenseDetail/ExpenseConcernDetailTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ExpenseConcernDetailTableViewController.swift
+//  ProjetSwift
+//
+//  Created by Fatima Machhouri on 02/04/2019.
+//  Copyright © 2019 F&Y. All rights reserved.
+//
+
+import UIKit
+
+class ExpenseConcernDetailTableViewController: NSObject {
+
+}
